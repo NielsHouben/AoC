41da442df50975abf1ca3e7eca5f25e4a875f274
Niels Houben <niels.houben1@gmail.com>
2024-12-04 00:13:45 +0100

    feat: 2024-3-b

--- 2024/3/main.py
+++ 2024/3/main.py
@@ -9,10 +9,27 @@ with open(FILENAME, "r") as input_file:
 
 
 def parse_memory(memory):
+    do_sum = True
     sum = 0
     idx = 0
     while True:
-        idx = memory.find("mul(", idx+1)
+        next_do = memory.find("do()", idx+1)
+        next_do_not = memory.find("don't()", idx+1)
+        next_mul = memory.find("mul(", idx+1)
+
+        findings = [x for x in [next_do, next_do_not, next_mul] if x != -1]
+        if len(findings) == 0:
+            break
+
+        idx = min(findings)
+
+        if idx == next_do:
+            do_sum = True
+            continue
+        if idx == next_do_not:
+            do_sum = False
+            continue
+
         if idx == -1 or idx + 4 > len(memory):
             break
 
@@ -24,7 +41,8 @@ def parse_memory(memory):
         n1, n2 = args
         if len(n1) > 3 or len(n2) > 3:
             continue
-        sum += int(n1) * int(n2)
+        if do_sum:
+            sum += int(n1) * int(n2)
 
     return sum
 

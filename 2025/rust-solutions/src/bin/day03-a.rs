e37d0a043b9fd580c58e04e07d89cd7c39f2e1fd
Niels Houben <niels.houben1@gmail.com>
2025-12-12 17:28:37 +0100

    refactor: functions

--- 2025/rust-solutions/src/bin/day03-a.rs
+++ 2025/rust-solutions/src/bin/day03-a.rs
@@ -11,43 +11,50 @@ fn main() {
 
     let data = fs::read_to_string(PUZZLE_FILENAME).expect("Should be able to read input file");
 
-    let mut val1: u32;
-    let mut val2: u32;
-    let mut potential_val2: u32;
+    println!("{}", sum_of_largets_joltages(data));
+}
+
+fn sum_of_largets_joltages(data: String) -> u32 {
     let mut res: u32 = 0;
 
     for line in data.split("\n") {
-        val1 = 0;
-        val2 = 0;
-        potential_val2 = 0;
         let mut iter = line.chars().rev();
         let Some(last) = iter.next() else {
             continue;
         };
-
         let last = last.to_digit(10).expect("Only digits in input");
-
-        for character in iter {
-            let digit = character.to_digit(10).expect("Only digits in input");
-            if digit > val1 {
-                val2 = val1;
-                val1 = digit;
-            } else if digit > val2 {
-                potential_val2 = digit;
-            }
-            if digit == val1 {
-                val2 = cmp::max(val2, potential_val2);
-            }
-        }
-
-        val2 = cmp::max(val2, last);
-
-        let mut o: String = "".to_owned();
-        o.push_str(val1.to_string().as_str());
-        o.push_str(val2.to_string().as_str());
-
-        res += o.parse::<u32>().unwrap();
+        res += largest_joltage(iter, last);
     }
 
-    println!("{}", res);
+    res
+}
+
+fn largest_joltage(iter: impl Iterator<Item = char>, last: u32) -> u32 {
+    let mut val1: u32;
+    let mut val2: u32;
+    let mut potential_val2: u32;
+    val1 = 0;
+    val2 = 0;
+    potential_val2 = 0;
+
+    for character in iter {
+        let digit = character.to_digit(10).expect("Only digits in input");
+        if digit > val1 {
+            val2 = val1;
+            val1 = digit;
+        } else if digit > val2 {
+            potential_val2 = digit;
+        }
+        if digit == val1 {
+            val2 = cmp::max(val2, potential_val2);
+        }
+    }
+
+    val2 = cmp::max(val2, last);
+
+    let mut o: String = "".to_owned();
+    o.push_str(val1.to_string().as_str());
+    o.push_str(val2.to_string().as_str());
+
+    o.parse::<u32>().unwrap()
 }
